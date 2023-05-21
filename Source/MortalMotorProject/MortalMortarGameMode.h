efcac10452d39c781aa48af91b58c92720eee4b4
kirillyakubov101 <59371020+kirillyakubov101@users.noreply.github.com>
2023-05-20 22:09:47 -0400

    2nd iteration adding skillset

--- Source/MortalMotorProject/MortalMortarGameMode.h
+++ Source/MortalMotorProject/MortalMortarGameMode.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "CoreSkill.h"
 #include "GameFramework/GameModeBase.h"
 #include "MortalMortarGameMode.generated.h"
 
@@ -15,14 +16,44 @@ class MORTALMOTORPROJECT_API AMortalMortarGameMode : public AGameModeBase
 	GENERATED_BODY()
 
 protected:
+	virtual void InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage) override;
 	virtual void BeginPlay() override;
 
 private:
 	APlayerController* m_PlayerController;
 	class APlayerMotorCar* m_Player;
 
+	void InitAllCoreSkills();
+
+	
+
 public:
 	UFUNCTION(BlueprintImplementableEvent)
 	void ActivateSkillUI();
-	
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void ActivateMinigun();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void ActivateNitro();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void ActivateFlameThrower();
+
+	UPROPERTY()
+	FCoreSkill CoreSkill_1;
+
+	UPROPERTY()
+	FCoreSkill CoreSkill_2;
+
+	UPROPERTY()
+	FCoreSkill CoreSkill_3;
+
+	UPROPERTY()
+	TArray<FCoreSkill> m_AllCoreSkills;
+
+	FCoreSkill* GetRandomCoreSkillFromList();
+
+	void AddCoreSkillToList(const FCoreSkill& newSkill);
+	void RemoveSelectedCoreSkill(FCoreSkill& SkillToRemove);
 };
---
+++ Source/MortalMotorProject/SkillsWidget.h
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CoreSkill.h"
+#include "SkillsWidget.generated.h"
+
+UCLASS()
+class MORTALMOTORPROJECT_API USkillsWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void NativeConstruct() override;
+
+	UFUNCTION(BlueprintCallable)
+	void InitWidget();
+
+	UFUNCTION()
+	void ActivateButton_1();
+
+	UFUNCTION()
+	void ActivateButton_2();
+
+	UFUNCTION()
+	void ActivateButton_3();
+
+private:
+	class APlayerMotorCar* Player;
+	class AMortalMortarGameMode* GameMode;
+
+	class UButton* m_Btn1;
+	class UTextBlock* m_Btn1Text;
+
+	class UButton* m_Btn2;
+	class UTextBlock* m_Btn2Text;
+
+	class UButton* m_Btn3;
+	class UTextBlock* m_Btn3Text;
+
+	FOnSkillAction m_btn1Action;
+	FOnSkillAction m_btn2Action;
+	FOnSkillAction m_btn3Action;
+
+	TArray<FCoreSkill> ActiveSkills;
+
+	UFUNCTION()
+	void InitSkillChoices();
+
+	
+	
+};
