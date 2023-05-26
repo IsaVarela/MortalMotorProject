38a923a975682c97b69303115c3d144d728e678c
kirillyakubov101 <59371020+kirillyakubov101@users.noreply.github.com>
2023-05-26 15:52:51 -0400

    Revert "Merge pull request #39 from Walking-Glitch/Juan"
    This reverts commit 2390ff6fc91c16e761e9e589d847ea8094d565d0, reversing
    changes made to 6f7652145b79d957bf9c850d155bb77b99723e87.

--- Source/MortalMotorProject/SkillsWidget.cpp
+++ Source/MortalMotorProject/SkillsWidget.cpp
@@ -5,10 +5,10 @@
 #include "Components/Button.h"
 #include "MortalMortarGameMode.h"
 #include "Components/TextBlock.h"
-#include "PlayerMotorCar.h"
 
 void USkillsWidget::NativeConstruct()
 {
+	InitWidget();
 }
 
 void USkillsWidget::BeginDestroy()
@@ -19,13 +19,7 @@ void USkillsWidget::BeginDestroy()
 void USkillsWidget::InitWidget()
 {
 	GameMode = Cast<AMortalMortarGameMode>(GetWorld()->GetAuthGameMode());
-	Player = Cast<APlayerMotorCar>(GetWorld()->GetFirstPlayerController()->GetPawn());
-
-	if (Player)
-	{
-		Player->OnLevelUpDelegate.AddUFunction(this, FName("InitSkillChoices"));
-	}
-
+	
 	//---------------BUTTON 1------------------------------------
 	UWidget* ButtonWidget_1 = GetWidgetFromName(TEXT("Btn1"));
 	if (ButtonWidget_1)
@@ -95,20 +89,23 @@ void USkillsWidget::InitWidget()
 
 void USkillsWidget::ActivateButton_1()
 {
-	m_btn1Action.ExecuteIfBound();
 	RestoreUnselectedSkills(m_btn1Action);
+	m_btn1Action.ExecuteIfBound();
+	
 }
 
 void USkillsWidget::ActivateButton_2()
 {
-	m_btn2Action.ExecuteIfBound();
 	RestoreUnselectedSkills(m_btn2Action);
+	m_btn2Action.ExecuteIfBound();
+	
 }
 
 void USkillsWidget::ActivateButton_3()
 {
-	m_btn3Action.ExecuteIfBound();
 	RestoreUnselectedSkills(m_btn3Action);
+	m_btn3Action.ExecuteIfBound();
+	
 }
 
 void USkillsWidget::RestoreUnselectedSkills(FOnSkillAction& skillAction)
@@ -117,7 +114,7 @@ void USkillsWidget::RestoreUnselectedSkills(FOnSkillAction& skillAction)
 	
 	for (const auto ele : m_Skills)
 	{
-		if (ele->OnSkillActionDelegate.GetHandle() != skillAction.GetHandle())
+		if (!ele->bIsGenericSkill && ele->OnSkillActionDelegate.GetHandle() != skillAction.GetHandle())
 		{
 			GameMode->AddSkillsToPool(ele);
 		}
