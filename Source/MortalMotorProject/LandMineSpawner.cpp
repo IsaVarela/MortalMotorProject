c36a7e51c640764b3da446447f0da4fa428a89f7
kirillyakubov101 <59371020+kirillyakubov101@users.noreply.github.com>
2023-05-26 20:26:07 -0400

    Mines 1st Iteration

---
+++ Source/MortalMotorProject/LandMineSpawner.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LandMineSpawner.h"
+
+// Sets default values
+ALandMineSpawner::ALandMineSpawner()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ALandMineSpawner::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ALandMineSpawner::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
