65ee49bf91a618be7ccd5bc52f57ac216bb5902e
Walking-Glitch <84610569+Walking-Glitch@users.noreply.github.com>
2023-06-03 01:09:16 -0400

    delete
    + delete everything to remove conflicts

--- Source/MortalMotorProject/CoreSkill.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "CoreSkill.h"
-
-const TArray<TSharedPtr<FCoreSkill>>& FCoreSkill::GetAllUpgrades() const
-{
-	return CoreSkillUpgrades;
-}
