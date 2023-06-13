7008508a9d6963c3af4ceb909bc96fc65014b1ee
kirillyakubov101 <59371020+kirillyakubov101@users.noreply.github.com>
2023-06-13 14:23:32 -0400

    First Iteration Sound Options

---
+++ Source/MortalMotorProject/Public/MotorGameInstance.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "MotorGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MORTALMOTORPROJECT_API UMotorGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void Init() override;
+
+public:
+	UMotorGameInstance();
+
+	UFUNCTION(BlueprintPure)
+	float GetMasterVolume() const;
+
+	UFUNCTION(BlueprintPure)
+	float GetSFX_Volume() const;
+
+	UFUNCTION(BlueprintCallable)
+	void SetMasterVolume(float value);
+
+	UFUNCTION(BlueprintCallable)
+	void SetSFX_Volume(float value);
+	
+private:
+	float m_masterVolume;
+	float m_sfxVolume;
+};
