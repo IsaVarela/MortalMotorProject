770b61432b38908d770c0102da34de9c3385afd8
Walking-Glitch <84610569+Walking-Glitch@users.noreply.github.com>
2023-06-15 11:34:35 -0400

    Enemy02 testing animations and moving behavior
    + setting up of anim blueprint, anim blendspace
    + testing of moving behavior

--- Source/MortalMotorProject/Public/ZombieSmasher.h
+++ Source/MortalMotorProject/Public/ZombieSmasher.h
@@ -17,10 +17,26 @@ class MORTALMOTORPROJECT_API AZombieSmasher : public AZombieRunner
 public:
 	AZombieSmasher();
 
+	
+
 protected:
 
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaSeconds) override;
 	virtual void ChasePlayer(const FVector& TargetLocation) const override;
 	void PositionToPlayer();
+	void Attack() const;
+	void Block() const;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category ="AI")
+		float BlockingRadius;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI")
+		float AttackRadius;
+
+	UPROPERTY()
+		float DotProduct;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animation")
+		TArray<UAnimMontage*> AttackAnims;
 };
